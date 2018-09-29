15b27fb3e365ead6a38bdf1fc721f4a7b563250a
李林溪 <luigili@yeah.net>
2018-09-28 23:21:49 -0600

    add getshorty

---
+++ GetShorty/GetShorty/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetShorty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
