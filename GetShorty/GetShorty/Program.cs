a70eafd50f2460009e4b5b248caec6c010341fd9
李林溪 <luigili@yeah.net>
2018-10-02 03:18:54 -0600

    testing heao

--- GetShorty/GetShorty/Program.cs
+++ GetShorty/GetShorty/Program.cs
@@ -7,68 +7,184 @@ using System.Threading.Tasks;
 
 namespace GetShorty
 {
+    public class PiorityQueue
+    {
+        public List<QueueNode> verts;
+        public PiorityQueue()
+        {
+            verts = new List<QueueNode>();
+        }
+        public int getsize()
+        {
+            return verts.Count;
+        }
+        public void swap(int n, int m)
+        {
+            QueueNode temp = verts[n];
+            verts[n] = verts[m];
+            verts[m] = temp;
+        }
+        public void swim(int pos)
+        {
+            while (pos > 0)
+            {
+                int s = pos / 2; //get its parent
+                if (verts[pos].toll < verts[s].toll&& verts[pos].name != verts[s].name)
+                {
+                    swap(pos, pos / 2);
+
+                }
+                else if (verts[pos].name == verts[s].name)
+                {
+                    throw new Exception();
+
+                 } //move to the top
+                pos = s;
+
+            }
+        }
+        public int inqueue(int tgtnanme)
+        {
+            for(int i=0;i<verts.Count;i++)
+            {
+                if (verts[i].name == tgtnanme)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public void update(QueueNode newvalue, int serial)
+        {
+            verts[serial] = newvalue;
+            int parent = serial / 2;
+            int leftchild = serial * 2 + 1;
+            if (verts[serial].toll < verts[parent].toll)
+            {
+                swim(serial);
+            }
+            else if (leftchild<verts.Count-1&&verts[serial].toll > verts[leftchild].toll)
+            {
+                sink(serial);
+            }
+        }
+        public void add(QueueNode newvalue)
+        {
+            verts.Add(newvalue);
+            swim(verts.Count-1);
+        }
+        public void sink(int position)//sink from the top to the bottom
+        {
+            while (position < verts.Count - 1)
+            {
+                int s = 2 * position + 1; //left child
+                if (s < verts.Count - 1)
+                {
+                    if (verts[s].toll < verts[position].toll && verts[s + 1].toll > verts[s].toll)
+                    {
+                        swap(position, s);
+                    }
+                    else if (verts[s + 1].toll < verts[position].toll&& verts[s + 1].toll < verts[s].toll)
+                    {
+                        swap(position, s + 1);
+                        s = s + 1;
+                    }
+                    else if (verts[s + 1].toll < verts[position].toll && verts[s + 1].toll < verts[position].toll)
+                    {
+                        swap(position, s );
+                        
+                    }
+
+                }
+                else if (s == verts.Count - 1)
+                {
+                    if (verts[s].toll < verts[position].toll)
+                    {
+                        swap(position, s);
+                    }
+                }
+                position = s;
+            }
+
+        }
+        public QueueNode poptop()
+        {
+            int start = 0;
+            int end = verts.Count - 1;
+            QueueNode result = verts[start];
+            swap(start, end);
+
+            verts.Remove(verts[end]);
+            sink(0);
+
+            return result;
+        }
+    }
+    public class QueueNode
+    {
+        public double toll;
+        public int name;
+        public QueueNode(double factor, int no, double tsf)
+        {
+            this.toll = 1-factor*tsf;
+            name = no;
+        }
+        public QueueNode(double toll, int no)
+        {
+            this.toll = toll;
+            name = no;
+        }
+    }
     public class SimpleGraph
     {
         Dictionary<int,Node> Paths;
         int nodes;
-        int[] prevs;
+ 
         double[] tolls;
-        int[] nexts;
-        double[] factors;
+        
         List<int> visited = new List<int>();
-        List<int> unvisited = new List<int>();
-
-        public void findexit()
+        
+        PiorityQueue unvisiteds = new PiorityQueue();
+        public void dijkstra()
         {
-
-            evaluate(0, 1);
-            tolls[0] = 1;
-            unvisited.Remove(0);
-            while (unvisited.Count!=0)
+            tolls[0] = 0;
+            unvisiteds.add(new QueueNode(0, 0));
+            while (unvisiteds.getsize() != 0)
             {
-                int s = getmost();
-                evaluate(s,tolls[s]);
-                unvisited.Remove(s);
-            }            
+                int nextnode = unvisiteds.poptop().name;
+                visited.Add(nextnode);
+                calculate(nextnode);
+            }
         }
-        public int getmost()
+        public void calculate(int name)
         {
-            double largest=tolls[unvisited[0]];
-            int s = 0;
-            for (int i = 0; i < unvisited.Count; i++)
+            foreach (KeyValuePair<int, double> neighbors in this.Paths[name].nexts)
             {
-                if (largest < tolls[unvisited[i]])
+                if (!visited.Contains(neighbors.Key))
                 {
-                    largest = tolls[unvisited[i]];
-                    s = i;
+                    if(tolls[neighbors.Key] == -1 ||
+                    tolls[neighbors.Key] > (1 - ((1 - tolls[name]) * neighbors.Value)))
+                    {
+                        tolls[neighbors.Key] = (1 - ((1 - tolls[name]) * neighbors.Value));
+
+                        if (unvisiteds.inqueue(neighbors.Key) != -1)
+                        {
+                            unvisiteds.update(new QueueNode(tolls[neighbors.Key], neighbors.Key), unvisiteds.inqueue(neighbors.Key));
+                        }
+                        else if (unvisiteds.inqueue(neighbors.Key) == -1)
+                        {
+                            unvisiteds.add(new QueueNode(tolls[neighbors.Key], neighbors.Key));
+                        }
+                    }
                 }
             }
-            if (largest == -1)
-            {
-                return unvisited[0];
-            }
-            else
-            {
-                return unvisited[s];
-            }
         }
-        public void evaluate(int start, double toll)
-        {
-
-            foreach (KeyValuePair<int, double> neighbors in this.Paths[start].nexts)
-            {
-                if (tolls[neighbors.Key] < toll*neighbors.Value)
-                {
-                    tolls[neighbors.Key] = toll * neighbors.Value;
-                }
-            }
-            
-
-        }
+       
+       
         public double GetDistance()
         {
-            findexit();
-            return double.Parse(tolls[nodes - 1].ToString());
+            dijkstra();
+            return double.Parse((1-tolls[nodes - 1]).ToString());
         }
         public SimpleGraph(string structure)
         {
@@ -76,44 +192,43 @@ namespace GetShorty
             int nodecount = int.Parse(datas[0]);
             nodes = nodecount;
             int lines = int.Parse(datas[1]);
-            prevs = new int[nodecount];
+          
             tolls = new double[nodecount];
-            nexts = new int[nodecount];
-            factors = new double[nodecount];
+
+           
             for (int k = 0; k < nodecount;k++)
             {
-                prevs[k] = -1;
+               
                 tolls[k] = -1;
-                nexts[k] = -1;
-                factors[k] = -1;
-                unvisited.Add(k);
+
 
             }
             Paths = new Dictionary<int, Node>();
             int i = 0;
-            while ( i < lines)
+            while (i < lines)
             {
                 string line = Console.ReadLine();
                 string[] datasu = line.Split(' ');
                 int start = int.Parse(datasu[0]);
                 int end = int.Parse(datasu[1]);
-                double toll = double.Parse(datasu[2]);
+                double factor = double.Parse(datasu[2]);
                 if (Paths.ContainsKey(start))
                 {
-                    Paths[start].AddNext(end, toll);
+                    Paths[start].AddNext(end, factor);
                 }
-                else
+                else if (!Paths.ContainsKey(start))
                 {
-                    Paths.Add(start,new Node(start,end,toll));
-                    
+                    Paths.Add(start, new Node(start, end, factor));
+
                 }
+                
                 if (Paths.ContainsKey(end))
                 {
-                    Paths[end].AddNext(start, toll);
+                    Paths[end].AddNext(start, factor);
                 }
-                else
+                else if (!Paths.ContainsKey(end))
                 {
-                    Paths.Add(end, new Node(end, start, toll));
+                    Paths.Add(end, new Node(end, start, factor));
 
                 }
                 i++;
@@ -121,17 +236,16 @@ namespace GetShorty
         }
         public class Node
         {
-            public int serial;
-            public bool visited;
-            public double tollsofar;
+
+            
+
             public Dictionary<int,double> nexts;
             public Node(int seiral, int end, double toll)
             {
                 nexts = new Dictionary<int, double>();
                 nexts.Add(end, toll);
-                this.serial = seiral;
-                tollsofar = -1;
-                visited = false;
+
+               
             }
            
             public void AddNext(int end,double toll)
@@ -148,8 +262,11 @@ namespace GetShorty
         static void Main(string[] args)
         {
             List<double> solutions = new List<double>();
+
+           
             
             string x = Console.ReadLine();
+            
             while (x != "0 0")
             {
                 SimpleGraph graph = new SimpleGraph(x);
@@ -161,6 +278,7 @@ namespace GetShorty
             {
                 Console.WriteLine(String.Format("{0:F4}", answer));
             }
+            
             Console.Read();
         }
     }
---
+++ heap/heap/Program.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace april_19
+{
+    class MaxHeap
+    {
+        List<int> element;
+        public int this[int index]
+        {
+            get { return element[index + 1]; }
+        }
+        public void buildheap(List<int> k)
+        {
+            foreach (int m in k)
+            {
+                add(m);
+            }
+        }
+        public int count()
+        {
+            return element.Count;
+        }
+        public void showvalue()
+        {
+            foreach (int k in element)
+            {
+                Console.WriteLine(k);
+            }
+        }
+        public MaxHeap()
+        {
+            element = new List<int>();
+
+        }
+        public void swap(int i, int m)
+        {
+            int temp = element[i];
+            element[i] = element[m];
+            element[m] = temp;
+        }
+        public void swim()
+        {
+            for (int i = element.Count - 1; i > 0;)
+            {
+                int s = i / 2;
+                if (element[i] > element[s])
+                {
+                    swap(i, i / 2);
+
+                }
+                i = s;
+
+            }
+        }
+        public void add(int newvalue)
+        {
+            element.Add(newvalue);
+            swim();
+        }
+        public void heapify()
+        {
+            List<int> result = new List<int>();
+            foreach (int k in element)
+            {
+                result.Add(k);
+            }
+            element.Clear();
+            buildheap(result);
+            sort();
+            element.Reverse();
+
+        }
+        public void sort()
+        {
+            int start = 0;
+            int end = element.Count - 1;
+            while (end > 0)
+            {
+                swap(start, end);
+
+                end -= 1;
+
+
+                for (int i = 0; i < end;)
+                {
+                    int s = 2 * i + 1;
+                    if (s < end)
+                    {
+                        if (element[s] > element[i] && element[s + 1] < element[s])
+                        {
+                            swap(i, s);
+                        }
+                        else if (element[s + 1] > element[i] && element[s + 1] > element[s])
+                        {
+                            swap(i, s + 1);
+                            s = s + 1;
+                        }
+
+                    }
+                    if (s == end)
+                    {
+                        if (element[s] > element[i])
+                        {
+                            swap(i, s);
+                        }
+                    }
+                    i = s;
+                }
+
+            }
+        }
+        public void sink()
+        {
+            for (int i = 0; i < element.Count - 1;)
+            {
+                int s = 2 * i + 1;
+                if (s < element.Count - 1)
+                {
+                    if (element[s] > element[i] && element[s + 1] < element[s])
+                    {
+                        swap(i, s);
+                    }
+                    else if (element[s + 1] > element[i] && element[s + 1] > element[s])
+                    {
+                        swap(i, s + 1);
+                        s = s + 1;
+                    }
+
+                }
+                if (s == element.Count - 1)
+                {
+                    if (element[s] > element[i])
+                    {
+                        swap(i, s);
+                    }
+                }
+                i = s;
+            }
+
+        }
+        public int poptop()
+        {
+            int start = 0;
+            int end = element.Count - 1;
+            int result = element[start];
+            swap(start, end);
+
+            element.Remove(element[end]);
+            sink();
+
+            return result;
+        }
+        public void testivairaiant()
+        {
+            for (int i = 0; i < element.Count - 1; i++)
+            {
+                int left = 2 * i + 1;
+                int right = 2 * i + 2;
+                if (left <= element.Count - 1)
+                {
+                    Debug.Assert(element[i] > element[left]);
+                    if (right <= element.Count - 1)
+                    {
+                        Debug.Assert(element[i] > element[right]);
+                    }
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<int> m = new List<int> { 100, 2, 40, 30, 1 };
+            foreach (int value in m)
+            {
+                Console.Write(value + ", ");
+            }
+
+
+            MaxHeap k = new MaxHeap();
+            Console.WriteLine("\nuse the above list to build a heap");
+
+            k.buildheap(m);
+            k.showvalue();
+            Console.WriteLine("add");
+            k.add(1000);
+            k.add(77);
+            k.add(88);
+
+            k.add(65535);
+            Console.WriteLine("test invariant");
+            k.testivairaiant();
+            Console.WriteLine("success");
+            k.showvalue();
+            Console.WriteLine("sort");
+            k.sort();
+            for (int i = 0; i < k.count() - 2; i++)
+                Debug.Assert(k[i] < k[i + 1]);
+            k.showvalue();
+
+            Console.WriteLine("reheap");
+            k.heapify();
+            k.showvalue();
+            Console.WriteLine("poptop");
+            List<int> poplist = new List<int>();
+            while (k.count() > 0)
+            {
+                int top = k.poptop();
+                Console.WriteLine(top);
+                poplist.Add(top);
+
+            }
+            for (int i = 0; i < poplist.Count - 2; i++)
+            {
+                Debug.Assert(poplist[i] > poplist[i + 1]);
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
